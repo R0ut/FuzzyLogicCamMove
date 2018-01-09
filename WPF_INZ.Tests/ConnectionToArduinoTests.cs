d73167a7bc0ec358b497dbae4abed3c2f485b56a
R0ut <kkaacczzoorr@gmail.com>
2018-01-09 22:12:20 +0100

    Add unit test project. Git ignore edit

---
+++ WPF_INZ.Tests/ConnectionToArduinoTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ServiceModule.Connection;
+
+namespace WPF_INZ.Tests
+{
+    [TestClass]
+    public class ConnectionToArduinoTests
+    {
+        [TestMethod]
+        public void ConnectionToArduinoShouldInit()
+        {
+            ConnectionToArduino con = new ConnectionToArduino();
+            Assert.IsNotNull(con.myPort);
+        }
+    }
+}
